e1094c6d1c772e56627c0d4ccc22ebf8d41a7d1f
dominant-28 <sohamkale2828@gmail.com>
2026-09-16 23:00:41 +0530

    chore: scaffold admin-api, gateway-node, frontend, test-backends

---
+++ gateway-node/src/main/java/com/gatewayplatform/gatewaynode/GatewayNodeApplication.java
@@ -0,0 +1,13 @@
+package com.gatewayplatform.gatewaynode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayNodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayNodeApplication.class, args);
+    }
+
+}
---
+++ gateway-node/src/test/java/com/gatewayplatform/gatewaynode/GatewayNodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gatewayplatform.gatewaynode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayNodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
